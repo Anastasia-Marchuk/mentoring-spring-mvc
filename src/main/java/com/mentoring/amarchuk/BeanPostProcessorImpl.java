4154d3720b66a73cb83df4a2dfffed74844a24b2
Anastasia <anastasiya_epam1@copyright.com>
2021-07-14 23:29:43 +0300

    first commit

---
+++ src/main/java/com/mentoring/amarchuk/BeanPostProcessorImpl.java
@@ -0,0 +1,18 @@
+package com.mentoring.amarchuk;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorImpl implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("BEFORE Initialization method called for: " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("AFTER Initialization method called for: " + beanName);
+        return bean;
+    }
+}
